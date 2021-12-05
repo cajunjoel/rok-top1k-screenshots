bffe76552cd8deaf1e2f4b98d6fb636f1ce6315a
Joel <git@joelrichard.com>
2021-12-04 19:20:59 -0500

    Adjusted management of settings

---
+++ settings.motoz3-1920.sh
@@ -0,0 +1,27 @@
+WINDOW_NAME="moto z3" # name of our ROK window
+
+# NOTE: All coordinates are from the upper-left corner 
+# of the display. (Positive X = right, Positive Y = down)
+
+# Window Geometry to place and position the Vysor window
+WIN_X=0            # X position for the upper-right corner
+WIN_Y=25           # Y position for the upper-right corner
+WIN_W=1920         # Window width in pixels
+WIN_H=1046         # Window height in pixels
+
+# Mouse Click Positions
+G1=(410 365)       # X Y coordinates to Open Governor 1
+G2=(410 470)       # X Y coordinates to Open Governor 2
+G3=(410 580)       # X Y coordinates to Open Governor 3
+G4=(410 700)       # X Y coordinates to Open Governor 4 (repeats)
+G999=(410 825)     # X Y coordinates to Open Governor 999
+G1000=(410 930)    # X Y coordinates to Open Governor 1000
+GHEIGHT=110        # Y pixels between two adjacent governor icon centers
+
+# Manipulate the governor's information pages
+GOVNAME=(833 368)  # X Y coordinates to Copy Name
+MORE=(522 836)     # X Y coordinates to Open More info
+KPOINTS=(1260 228) # X Y coordinates to Open Kill Points
+CMORE=(119 504)    # X Y coordinates to Close More Info
+CGOV=(119 504)     # X Y coordinates to Close Governor Profile
+
